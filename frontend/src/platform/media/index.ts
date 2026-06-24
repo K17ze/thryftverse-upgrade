0fed58495c136e070f92dd382796926d770d5b2c
k17ze <khaizemuhammad@gmail.com>
2026-06-24 01:10:51 +0100

    feat: complete creator media transforms

--- frontend/src/platform/media/index.ts
+++ frontend/src/platform/media/index.ts
@@ -1,2 +1,22 @@
-export { mediaTransforms, compressImage, resizeImage, cropImage, rotateImage, flipImage } from './mediaTransforms';
-export type { CompressOptions } from './mediaTransforms';
+export {
+  mediaTransforms,
+  compressImage,
+  resizeImage,
+  cropImage,
+  rotateImage,
+  flipImage,
+  normalizeImage,
+  resizeForUpload,
+  cropToAspectRatio,
+  createThumbnail,
+  exportImage,
+} from './mediaTransforms';
+export type {
+  CompressOptions,
+  MediaTransformResult,
+  MediaTransformFormat,
+  ResizePreset,
+  ResizePresetName,
+  CropAspectRatioOptions,
+  ExportImageOptions,
+} from './mediaTransforms';
